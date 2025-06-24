97ece489ab7c7be168fc858d4dc530abbb8a0fcb
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-06-24 10:42:05 +0300

    created cards models

---
+++ app/models/cards.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from app.db.base import Base
+from app.enums import CardType
+
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id = Column(Integer, primary_key=True)
+    card_number = Column(String(20), nullable=False)
+    exp_date = Column(String(20), nullable=False)
+    card_holder = Column(String(30), nullable=False)
+    cvv = Column(String(3), nullable=False)
+    card_type = Column(CardType, nullable=False)
+
+    # user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    # owner = relationship("User", back_populates="cards")
