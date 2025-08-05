42cd8144b493e829beefac70e3919f1cc8c02fae
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-08-06 02:36:20 +0300

    add user CRUD functions with password hashing and verification

---
+++ src/app/crud/user.py
@@ -0,0 +1,47 @@
+from sqlalchemy.orm import Session
+from app import models, schemas
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_user_by_phone(db: Session, phone: str):
+    return db.query(models.User).filter(models.User.phone == phone).first()
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(
+        username=user.username,
+        email=user.email,
+        phone=user.phone,
+        password_hash=hashed_password,
+        photo=user.photo,
+        is_approved=False,
+        is_admin=False
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, db_user: models.User, user_update: schemas.UserUpdate):
+    for var, value in vars(user_update).items():
+        if value is not None:
+            setattr(db_user, var, value)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
