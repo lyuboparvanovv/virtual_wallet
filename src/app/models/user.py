3bed3b435ddc013a665be3b1e28b2a198dfe2e9a
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-08-07 00:26:27 +0300

    update in user

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -7,22 +7,23 @@ from app.db.session import Base
 
 
 class User(Base):
-        __tablename__ = "users"
+    __tablename__ = "users"
 
-        id = Column(Integer, primary_key=True, index=True)
-        username = Column(String(20), unique=True, nullable=False, index=True)
-        password_hash = Column(String(255), nullable=False)
-        email = Column(String(255), unique=True, nullable=False, index=True)
-        phone = Column(String(10), unique=True, nullable=False, index=True)
-        photo = Column(String, nullable=True)
-        is_active = Column(Boolean, default=True)
-        is_admin = Column(Boolean, default=False)
-        is_approved = Column(Boolean, default=False)
-        created_at = Column(DateTime, default=datetime.utcnow)
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(20), unique=True, nullable=False, index=True)
+    password_hash = Column(String(255), nullable=False)
+    email = Column(String(255), unique=True, nullable=False, index=True)
+    phone = Column(String(10), unique=True, nullable=False, index=True)
+    photo = Column(String, nullable=True)
+    is_active = Column(Boolean, default=True)
+    is_admin = Column(Boolean, default=False)
+    is_approved = Column(Boolean, default=False)
+    created_at = Column(DateTime, default=datetime.now())
 
-        cards = relationship("Card", back_populates="user", cascade="all, delete-orphan")
-        transactions_sent = relationship("Transaction", foreign_keys='Transaction.sender_id', back_populates="sender")
-        transactions_received = relationship("Transaction", foreign_keys='Transaction.receiver_id',
-                                             back_populates="receiver")
-        contacts = relationship("Contact", back_populates="owner", cascade="all, delete-orphan")
-        categories = relationship("Category", back_populates="user", cascade="all, delete-orphan")
+    cards = relationship("Card", back_populates="user", cascade="all, delete-orphan")
+    transactions_sent = relationship("Transaction", foreign_keys='Transaction.sender_id', back_populates="sender")
+    transactions_received = relationship("Transaction", foreign_keys='Transaction.receiver_id',
+                                         back_populates="receiver")
+    contacts = relationship("Contact",back_populates="owner",foreign_keys="Contact.owner_id", cascade="all, delete-orphan"
+    )
+    categories = relationship("Category", back_populates="user", cascade="all, delete-orphan")
