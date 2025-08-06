80889a1dccaf5009f2454f9339ac1f32413b585a
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-08-07 00:33:16 +0300

    Configure SQLAlchemy engine, sessionmaker, Base, and provide get_db dependency

--- src/app/db/session.py
+++ src/app/db/session.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-from app.core.config import settings
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+import app.models
+from app.services.get_settings import get_settings
 
-DATABASE_URL = settings.DATABASE_URL
+settings = get_settings()
+DATABASE_URL = (f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}"
+                f"@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}")
 
 engine = create_engine(DATABASE_URL)
 
@@ -10,3 +13,18 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+Base.metadata.create_all(bind=engine)
+
+DB_CONFIG = {
+    "dbname": settings.DB_NAME,
+    "user": settings.DB_USER,
+    "password": settings.DB_PASSWORD,
+    "host": settings.DB_HOST,
+    "port": settings.DB_PORT
+}
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
