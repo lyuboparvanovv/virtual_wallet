4983abb88de7dc403acff7c061c833bc0bc19db5
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-08-06 18:32:48 +0300

    add user authentication

---
+++ src/app/dependancies/auth_d.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app import models
+from app.crud.user import *
+from app.db.session import SessionLocal
+from app.services import auth
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    username = auth.verify_access_token(token, credentials_exception)
+    user = get_user_by_username(db, username=username)
+    if not user or not user.is_active or not user.is_approved:
+        raise credentials_exception
+    return user
+
+def get_current_active_user(current_user: models.User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+def get_current_active_admin(current_user: models.User = Depends(get_current_user)):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=403, detail="Requires admin privileges")
+    return current_user
