ecedb92f4bad69fe103367e097ce6f81a03f8e5e
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-08-06 11:06:47 +0300

    update file

--- src/app/services/get_settings.py
+++ src/app/services/get_settings.py
@@ -1,46 +1,19 @@
-# mypy: disable-error-code="assignment"
-# See: https://github.com/pydantic/pydantic/issues/3143
-
 from functools import lru_cache
-from typing import List, Optional, Union
-from pydantic import AnyHttpUrl, field_validator, BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    API_V1_STR: str = "/api/v1"
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
-    VERSION: Optional[str] = "9.9.9.9"
-
-    @field_validator("BACKEND_CORS_ORIGINS", mode="before")
-    @classmethod
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        return v
 
     PROJECT_NAME: str = "Virtual-Wallet"
-    SUPER_SECRET_THING: str
-
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
-    SECRET_KEY: str
-
-    SMTP_SERVER: str
-    SMTP_PORT: int
-    SMTP_USERNAME: str
-    SMTP_PASSWORD: str
-    SMTP_FROM_ADDR: str
-    
-
 
     DB_USER: str
     DB_PASSWORD: str
     DB_HOST: str
     DB_PORT: str
     DB_NAME: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
-    OPENAI_API_KEY: str
 
 
     model_config = SettingsConfigDict(
