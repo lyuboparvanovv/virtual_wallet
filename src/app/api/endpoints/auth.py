c3aa6aef573c8ad971a564b32d6a097b8d1fb2ea
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-08-06 10:44:23 +0300

    register endpoint updated

--- src/app/api/endpoints/auth.py
+++ src/app/api/endpoints/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
+from app.crud.user import get_user_by_username, get_user_by_email, get_user_by_phone, create_user
 from app.db.session import get_db
 from app.schemas.user import UserOut, UserCreate
 from app.schemas.auth import *
@@ -9,7 +10,14 @@ router = APIRouter()
 
 @router.post("/register", status_code=status.HTTP_201_CREATED, response_model=UserOut)
 def register(user: UserCreate, db: Session = Depends(get_db)):
-    pass
+    if get_user_by_username(db, user.username):
+        raise HTTPException(status_code=400, detail="Username already exists")
+    if get_user_by_email(db, user.email):
+        raise HTTPException(status_code=400, detail="Email already exists")
+    if get_user_by_phone(db, user.phone):
+        raise HTTPException(status_code=400, detail="Phone already exists")
+    db_user = create_user(db, user)
+    return db_user
 
 @router.post("/token", response_model=Token)
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
