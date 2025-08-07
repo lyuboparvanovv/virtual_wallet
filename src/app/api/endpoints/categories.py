267a8682ccd824170f6bb660153fcba3b7daa7e6
Lyubo Parvanov <lyuboparvanovv@gmail.com>
2025-08-07 23:58:44 +0300

    add create category endpoint

---
+++ src/app/api/endpoints/categories.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.db.session import get_db
+from app.dependancies.auth_d import get_current_user
+from app.models import Category, User
+from app.schemas.category import CategoryOut, CategoryCreate
+
+router = APIRouter()
+
+@router.post("/", response_model=CategoryOut, status_code=status.HTTP_201_CREATED)
+def create_category(category: CategoryCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    db_category = Category(user_id=current_user.id, name=category.name)
+    db.add(db_category)
+    db.commit()
+    db.refresh(db_category)
+    return db_category
+
